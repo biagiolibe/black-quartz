7ce33735097a30dc349973d00f4d596a7170eaa2
biagiolibe <biagio.liberto@telepass.com>
2025-03-09 22:06:03 +0100

    add inital config for bevy

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,40 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
-    }
-}
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup)
         .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+fn setup(mut commands: Commands) {
+    // Camera
+    commands.spawn(Camera2d::default());
+
+    // Ground
+    commands.spawn((
+        Sprite {
+            color: Color::srgba(0.3, 0.5, 0.3, 0.2),
+            custom_size: Some(Vec2::new(500.0, 20.0)),
+            ..default()
+        },
+        RigidBody::Fixed,
+        Collider::cuboid(250.0, 10.0),
+    ));
+
+    // Drilling Machine (Player)
+    commands.spawn((
+        Sprite {
+            color: Color::srgba(0.8, 0.2, 0.2, 0.2),
+            custom_size: Some(Vec2::new(30.0, 30.0)),
+            ..default()
+        },
+        RigidBody::Dynamic,
+        Collider::cuboid(15.0, 15.0),
+        GravityScale(1.0),
+        Velocity::zero(),
+    ));
 }
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
